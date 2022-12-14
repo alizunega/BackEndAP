41628544cecf44a9715fa310077991275b648a85
alizunega <alizunega84@gmail.com>
2022-12-14 11:09:21 -0300

    actualizacion version SB

--- src/main/java/com/portfolio/backend/security/repository/iRolRepository.java
+++ src/main/java/com/portfolio/backend/security/repository/iRolRepository.java
@@ -3,12 +3,10 @@ package com.portfolio.backend.security.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.portfolio.backend.security.entity.Rol;
 import com.portfolio.backend.security.enums.RolNombre;
 
-@Repository
 public interface iRolRepository extends JpaRepository<Rol, Integer> {
     Optional<Rol> findByRolNombre(RolNombre rolNombre);
     
--- src/main/java/com/portfolio/backend/security/repository/iUsuarioRepository.java
+++ src/main/java/com/portfolio/backend/security/repository/iUsuarioRepository.java
@@ -3,11 +3,9 @@ package com.portfolio.backend.security.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.portfolio.backend.security.entity.Usuario;
 
-@Repository
 public interface iUsuarioRepository extends JpaRepository<Usuario, Integer>{
     Optional<Usuario> findByNombreUsuario(String nombreUsuario);
     
