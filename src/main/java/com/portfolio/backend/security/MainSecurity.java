18e38b50d30ba98c27e37f043e9564d63a501d2f
alizunega <alizunega84@gmail.com>
2023-01-30 17:15:28 -0300

    cambios commit 30-01

--- src/main/java/com/portfolio/backend/security/MainSecurity.java
+++ src/main/java/com/portfolio/backend/security/MainSecurity.java
@@ -20,7 +20,7 @@ import com.portfolio.backend.security.service.UserDetailsImpl;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MainSecurity {
     @Autowired
     UserDetailsImpl userDetailsImpl;
